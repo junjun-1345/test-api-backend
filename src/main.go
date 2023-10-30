2d20ea1a91b0bd2a3d3a211a322378d642499377
junjun-1345 <junjun.sekaowa.1345@ezweb.ne.jp>
2023-10-31 01:39:08 +0900

    ✨ add MySQL

--- src/main.go
+++ src/main.go
@@ -3,16 +3,33 @@ package main
 import (
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
+type Product struct {
+	gorm.Model
+	Code string
+	Price uint
+}
+
 func main() {
+	dsn := "root:password@tcp(mysql_db)/dev?charset=utf8mb4&parseTime=True&loc=Local"
+	db, _ := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	db.AutoMigrate(&Product{})
+
+	db.Create(&Product{Code: "D42", Price: 100})
+
+	var product Product
+	db.First(&product, 1)
+
 	router:= gin.Default()
 	router.Use(cors.Default())
 
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"id": "abjcabcajk",
-			"name": "じゅんじゅん",
+			"code": product,
 		})
 	})
 	router.Run(":8080")
